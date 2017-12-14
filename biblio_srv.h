de4fb214dfd88cb6d24edea775f9e423bc735f9c
eleve <eleve@S206PC11.priv.ig2i.fr>
2017-12-14 11:44:43 +0100

    Envoi et recpetion requete correcte

--- biblio_srv.h
+++ biblio_srv.h
@@ -2,14 +2,14 @@
 
 #define MAX_CLIENT 50
 
-struct client{
+typedef struct{
 	char pseudo[MAX_PSEUDO]; //le pseudo
 	int mode;	//le mode
 	int etat;  //etat 0: libre  etat 1: occupé
-	struct sockaddr_in; //contient la socket du client
-}
+	struct sockaddr_in socketClient; //contient la socket du client
+} client;
 
 
 //PROTOTYPES
-void dialogueClient(int);
+void dialogueClient(int,int*,client*);
 void derouter(int);
